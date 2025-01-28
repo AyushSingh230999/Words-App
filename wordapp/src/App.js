376dcd6e2e0025515789ce7a1f07ff806ac316f8
AyushSingh230999 <110291322+AyushSingh230999@users.noreply.github.com>
2025-01-28 16:47:22 +0530

    Add files via upload

---
+++ wordapp/src/App.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <>
+    <Search/>
+    </>
+
+  );
+}
+
+export default App;
